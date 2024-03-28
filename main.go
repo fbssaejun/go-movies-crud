95a1c245ceeb9e863e460f22e70c62455e19dbdc
Anthony Kim <fbssaejun@gmail.com>
2024-03-28 16:05:25 -0400

    initialize router and route handlers

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Moive struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +18,19 @@ type Moive struct {
 type Director struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
+}
+
+var movies []Moive
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
