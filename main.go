2d55a95b9c74d0127be6d8469715b31580f833d9
Anthony Kim <fbssaejun@gmail.com>
2024-03-28 15:58:49 -0400

    create Movie and Director structs

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+type Moive struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
